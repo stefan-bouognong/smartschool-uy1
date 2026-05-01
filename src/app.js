53593618fd8ebf2076a66b4992d463acaeef64f8
yvanna Djaha <yvannadjaha@gmail.com>
2026-05-01 19:54:22 +0100

    mise en place des endpoint pour admin

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// Routes du module Admin
+const adminRoutes = require('./modules/admin/admin.routes');
+app.use('/api/admin', adminRoutes);
 // Route test
 app.get('/', (req, res) => {
   res.json({ message: 'SmartSchool API running' });
